e1d8b939fc02f7f4114de5b13e9eaef07a218a74
LukaszJasinski <jasinski.l@outlook.com>
2025-01-29 15:41:47 +0100

    Moved prefix definition

--- NewTenant.py
+++ NewTenant.py
@@ -6,10 +6,11 @@ class RunCommand(Script):
     class Meta:
         name = "Create a new Tenant"
         description ="todo"
-    prefix="100.64.0.0/10"
+    
 
     def run(self, data, commit):
-        prefix_obj = Prefix.objects.get(prefix=data['prefix'])
+        prefix="100.64.0.0/10"
+        prefix_obj = Prefix.objects.get(prefix=prefix)
         available_ips = prefix_obj.get_available_ips()
         for x in 20:
             print(available_ips[x])
