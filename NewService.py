18708f33842b4837406e5571201615b424d86bfd
LukaszJasinski <jasinski.l@outlook.com>
2025-02-28 20:52:32 +0100

    Testowy choice var

--- NewService.py
+++ NewService.py
@@ -5,6 +5,22 @@ class NewService(Script):
     class Meta:
         name = "List Available IPs from Fixed Prefix"
         description = "Lists the first available IP addresses from the fixed prefix 100.64.0.0/10 without reservation."
+        mask =(
+            ('29', '/29'),
+            ('28', '/28'),
+            ('27', '/27'),
+            ('26', '/26'),
+            ('25', '/25'),
+            ('24', '/24'),
+            ('23', '/23'),
+            ('22', '/22'),
+            ('21', '/21'),
+            ('20', '/20'),
+            ('19', '/19'),
+            ('18', '/18'),
+            ('17', '/17'),
+            ('16', '/16')
+        )
 
     def run(self, data, commit):
         prefix_obj=Prefix.objects.get(prefix="100.64.0.0/10")
