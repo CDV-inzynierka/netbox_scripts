7a7f43a1a4daf8034473156710a6376913560582
LukaszJasinski <jasinski.l@outlook.com>
2025-02-28 20:59:17 +0100

    Zmiana definicji ChoiceVar

--- NewService.py
+++ NewService.py
@@ -1,11 +1,14 @@
-from extras.scripts import Script
+from extras.scripts import Script,ChoiceVar
 from ipam.models import Prefix
 
 class NewService(Script):
     class Meta:
         name = "List Available IPs from Fixed Prefix"
         description = "Lists the first available IP addresses from the fixed prefix 100.64.0.0/10 without reservation."
-        mask =(
+        mask =ChoiceVar(
+            description = "Select a mask for new service",
+            label = "Mask",
+            choices=(
             ('29', '/29'),
             ('28', '/28'),
             ('27', '/27'),
@@ -20,14 +23,17 @@ class NewService(Script):
             ('18', '/18'),
             ('17', '/17'),
             ('16', '/16')
+            ),
+            default='29'
         )
-        mask=ChoiceVar(choices=mask)
 
 
     def run(self, data, commit):
         prefix_obj=Prefix.objects.get(prefix="100.64.0.0/10")
         available_ip = prefix_obj.get_available_ips()
+
         self.log_success("Available IPs in master prefix:", str(available_ip))
+        
         #if not available_ips:
         #    self.log_error("No available IP addresses in this prefix.")
         #    return
