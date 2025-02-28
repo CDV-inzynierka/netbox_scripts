eae03e045b17758e180a4e9047287f1bcdc0c4a1
LukaszJasinski <jasinski.l@outlook.com>
2025-02-28 21:00:34 +0100

    Poprawa wcięć

--- NewService.py
+++ NewService.py
@@ -5,27 +5,28 @@ class NewService(Script):
     class Meta:
         name = "List Available IPs from Fixed Prefix"
         description = "Lists the first available IP addresses from the fixed prefix 100.64.0.0/10 without reservation."
-        mask =ChoiceVar(
-            description = "Select a mask for new service",
-            label = "Mask",
-            choices=(
-            ('29', '/29'),
-            ('28', '/28'),
-            ('27', '/27'),
-            ('26', '/26'),
-            ('25', '/25'),
-            ('24', '/24'),
-            ('23', '/23'),
-            ('22', '/22'),
-            ('21', '/21'),
-            ('20', '/20'),
-            ('19', '/19'),
-            ('18', '/18'),
-            ('17', '/17'),
-            ('16', '/16')
-            ),
-            default='29'
-        )
+    
+    mask =ChoiceVar(
+        description = "Select a mask for new service",
+        label = "Mask",
+        choices=(
+        ('29', '/29'),
+        ('28', '/28'),
+        ('27', '/27'),
+        ('26', '/26'),
+        ('25', '/25'),
+        ('24', '/24'),
+        ('23', '/23'),
+        ('22', '/22'),
+        ('21', '/21'),
+        ('20', '/20'),
+        ('19', '/19'),
+        ('18', '/18'),
+        ('17', '/17'),
+        ('16', '/16')
+        ),
+        default='29'
+    )
 
 
     def run(self, data, commit):
