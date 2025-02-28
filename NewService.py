02d9c7c628af9073b6b11332195e903e29060e7a
LukaszJasinski <jasinski.l@outlook.com>
2025-02-28 11:49:19 +0100

    Casting to string

--- NewService.py
+++ NewService.py
@@ -10,7 +10,7 @@ class NewService(Script):
         prefix_obj=Prefix.objects.get(prefix="100.64.0.0/10")
         available_ip = prefix_obj.get_available_ips()
         
-        self.log_success("Available IPs in master prefix:", available_ip)
+        self.log_success("Available IPs in master prefix:", str(available_ip))
         #if not available_ips:
         #    self.log_error("No available IP addresses in this prefix.")
         #    return
