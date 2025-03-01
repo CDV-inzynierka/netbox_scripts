3009fb161e8363ee2b72da09b6445b93c5a8b8ca
LukaszJasinski <jasinski.l@outlook.com>
2025-03-01 14:32:17 +0100

    Dodanie Filtra

--- NewService.py
+++ NewService.py
@@ -52,7 +52,8 @@ class NewService(Script):
 
     def run(self, data, commit):
         PrefixObj=Prefix.objects.get(prefix=self.PARENT_PREFIX)
-        AvailableIP = PrefixObj.get_available_prefixes().filter(prefix_length=self.PrefixLength).first()
+        PrefixLenghtFilter=int(data[self.PrefixLenght])
+        AvailableIP = PrefixObj.get_available_prefixes().filter(prefix_length=PrefixLenghtFilter).first()
 
         self.log_success("First available prefix with given mask:", str(AvailableIP))
         
