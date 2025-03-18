83dd51bc845a058488b5fc3e1bead2a5df0a2b47
LukaszJasinski <jasinski.l@outlook.com>
2025-03-18 19:35:51 +0100

    podmiana filtra

--- DeleteService.py
+++ DeleteService.py
@@ -22,7 +22,7 @@ class NewService(Script):
         description = "Pick a prefix to delete",
         query_params = {
             'tenant_id': '$Client',
-            'role__name': 'Client'
+            'role': '1' #role Client in Prefixes
         }
     )
 
