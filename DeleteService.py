dda8c14cd8e3a9f3ee3bd791d7a4dce10595ebc0
LukaszJasinski <jasinski.l@outlook.com>
2025-04-06 18:48:01 +0200

    Dopisana logika usuwania konfiguracji interfejsu

--- DeleteService.py
+++ DeleteService.py
@@ -2,6 +2,7 @@ from extras.scripts import Script,ObjectVar
 from ipam.models import Prefix, VLAN
 from tenancy.models import Tenant
 from utilities.exceptions import AbortScript
+from dcim.models import Interface
 
 class NewService(Script):
     class Meta:
@@ -30,15 +31,36 @@ class NewService(Script):
         
         deleted_prefix=data['Prefix']
         deleted_vlan=VLAN.objects.get(name=deleted_prefix.description)
+        unset_interface=Interface.objects.get(untagged_vlan=deleted_vlan)
         try:
-            deleted_prefix.delete()
-            self.log_success(f"Succesfully deleted a prefix: {deleted_prefix.prefix}")
-        except Exception as e:
-            raise AbortScript(f"An error occured during deleting a prefix: {str(e)}") # type: ignore
+            try:
+                deleted_prefix.delete()
+                self.log_success(f"Succesfully deleted a prefix: {deleted_prefix.prefix}")
+            except Exception as e:
+                raise AbortScript(f"An error occured during deleting a prefix: {str(e)}") 
 
-        try:
-            deleted_vlan.delete()
-            self.log_success(f"Succesfully deleted a VLAN: {deleted_vlan.name} with ID: {str(deleted_vlan.vid)}")
+            try:
+                deleted_vlan.delete()
+                self.log_success(f"Succesfully deleted a VLAN: {deleted_vlan.name} with ID: {str(deleted_vlan.vid)}")
+            except Exception as e:
+                raise AbortScript(f"An error occured during deleting a vlan: {str(e)}") 
+
+            try:
+                if unset_interface.pk and hasattr(Interface,'snapshot'):
+                    unset_interface.snapshot()
+
+                unset_interface.untagged_vlan=None
+                unset_interface.mode=None
+                unset_interface.description=None
+                unset_interface.full_clean()
+                unset_interface.save()
+                self.log_success(f"Interface {unset_interface.name} successfully unbound")
+            except Exception as e:
+                raise AbortScript(f"An error occured during reconfiguring an interface: {str(e)}")
         except Exception as e:
-            raise AbortScript(f"An error occured during deleting a vlan: {str(e)}") # type: ignore
+            raise AbortScript(f"Deleting service unsuccesful")
+        
+        unset_interface.tags.set(['Free port'])
+        unset_interface.save()
+
 
